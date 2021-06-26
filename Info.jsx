aed2c56e8f7a9f0645d55f480cb31bb13d5fe56e
JesseIloka <66491002+JesseIloka@users.noreply.github.com>
2021-06-26 14:53:25 +0100

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  return (
+    <div class = "note">
+      <h1>Javascript and React JS</h1>
+      <p>Basic Web Dev React JS Bootcamp</p>
+    </div>
+  );
+}
+
+
+export default Info;
